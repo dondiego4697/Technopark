029daf9dc0553f853e2b6d901212dd991d9e3797
denis <dondiego4697@mail.ru>
2017-04-09 23:42:06 +0300

    cosmetic

---
+++ 1 sem/Algorithms and Data Structures/Module3/Task4-2/cmake-build-debug/CMakeFiles/Task4_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task4_2.dir/main.cpp.obj"
+  "Task4_2.pdb"
+  "Task4_2.exe"
+  "Task4_2.exe.manifest"
+  "libTask4_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task4_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
